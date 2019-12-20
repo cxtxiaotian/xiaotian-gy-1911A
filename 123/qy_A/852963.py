0a09a69fe239c31899868afc167e40856d87f5dd
chenxiaotian <2225910313@qq.com>
2019-12-20 16:46:41 +0800

    测试

--- 123/qy_A/852963.py
+++ 123/qy_A/852963.py
@@ -1,2 +1,3 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+print(84616547984)
--- 123/qy_A/modul.py
+++ 123/qy_A/modul.py
@@ -1,49 +1,51 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-# import json
-# j='''
-# {"name":"小明",
-# "sex":"未知",
-# "sex1":"未改造成功",
-# "现在为女":"已改造成功"
-# }
-# '''
-# d= json.loads(j)
-# d["sex"]="男"
+# #!/usr/bin/env python
+# # -*- coding:utf-8 -*-
+# # import json
+# # j='''
+# # {"name":"小明",
+# # "sex":"未知",
+# # "sex1":"未改造成功",
+# # "现在为女":"已改造成功"
+# # }
+# # '''
+# # d= json.loads(j)
+# # d["sex"]="男"
+# # print(d)
+# #
+# # j= json.dumps(d,ensure_ascii=False)
+# # print(j)
+# import random
+# #获取随机数字
+# a= random.randint(1,20)
+# print(a)
+#
+# #有序的可迭代对象 str list tuple
+# b= random.choice("123456879")
+# print(b)
+#
+# import os
+# #获取文件的绝对路径; _file_代表当前。py文件的文件名
+# #print(_file_)
+# p=os.path.abspath(__file__)
+# print(p)
+# #获取文件的绝对路径
+# p=os.path.abspath("test.txt")
+# print(p)
+# #获取文件夹路径
+# d = os.path.dirname(p)
 # print(d)
 #
-# j= json.dumps(d,ensure_ascii=False)
-# print(j)
-import random
-#获取随机数字
-a= random.randint(1,20)
-print(a)
-
-#有序的可迭代对象 str list tuple
-b= random.choice("123456879")
-print(b)
-
-import os
-#获取文件的绝对路径; _file_代表当前。py文件的文件名
-#print(_file_)
-p=os.path.abspath(__file__)
-print(p)
-#获取文件的绝对路径
-p=os.path.abspath("test.txt")
-print(p)
-#获取文件夹路径
-d = os.path.dirname(p)
-print(d)
-
-#获取文件名
-t=os.path.basename(p)
-print(t)
-
-#拼接目录和文件
-path=os.path.join(d,t)
-print(path)
-#创建文件夹
-os.mkdir("gy_B")
-print(path)
-
-os.system("ipconfig")
+# #获取文件名
+# t=os.path.basename(p)
+# print(t)
+#
+# #拼接目录和文件
+# path=os.path.join(d,t)
+# print(path)
+# #创建文件夹
+# os.mkdir("gy_B")
+# print(path)
+#
+# os.system("ipconfig")
+print(123456)
+# print(dsagfsdg)
